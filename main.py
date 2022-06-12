6149db60b1dda1df8f6573fdd87c3bb398bde48f
SaladirGaming <saladirgaming@gmail.com>
2022-06-12 23:47:16 +0200

    Login GUI
    Adding Login GUI without functionality

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.theming import ThemeManager
-from kivymd.uix.button import MDFillRoundFlatButton, MDFillRoundFlatIconButton
+from kivymd.uix.button import MDFillRoundFlatButton, MDFillRoundFlatIconButton, MDFlatButton
 from kivymd.uix.textfield import MDTextField
 from kivymd.uix.label import MDLabel
 from kivymd.uix.toolbar import MDToolbar
 from kivy.lang.builder import Builder
-
-KV = """
-MDTextField:
-    hint_text:'Enter Username'
-    pos_hint:{"center_x": 0.5, "center_y": 0.5}
-    size_hint_x:None
-    width;:300
+from helpers import username_helper, password_helper
+from kivymd.uix.dialog import MDDialog
 
 
-"""
+class Login(MDApp):
+    def build(self):
+        pass
 
 
 class MainApp(MDApp):
     def build(self):
         screen = MDScreen()
 
-        '''self.username = MDTextField(text="Enter Username",
-                                    pos_hint={"center_x": 0.5, "center_y": 0.5},
-                                    size_hint_x=None,
-                                    width=300
-                                    )'''
+        self.username = Builder.load_string(username_helper)
+        self.password = Builder.load_string(password_helper)
 
         self.toolbar = MDToolbar(title="Friendbook")
         self.toolbar.pos_hint = {"top": 1}
@@ -35,23 +29,48 @@ class MainApp(MDApp):
 
         # Buttons
         login_btn = MDFillRoundFlatButton(
-            text="login",
-            pos_hint={"center_x": 0.4, "center_y": 0.3}
-
+            text="Login",
+            pos_hint={"center_x": 0.4, "center_y": 0.3},
+            size_hint=(None, None),
+            on_release=self.check_data
         )
 
         reg_btn = MDFillRoundFlatButton(
-            text="login",
-            pos_hint={"center_x": 0.6, "center_y": 0.3}
+            text="Register",
+            pos_hint={"center_x": 0.6, "center_y": 0.3},
+            size_hint=(None, None)
 
         )
 
         # Add to Screen
-        screen.add_widget(KV)
+        screen.add_widget(self.username)
+        screen.add_widget(self.password)
         screen.add_widget(login_btn)
         screen.add_widget(reg_btn)
 
         return screen
+
+    def check_data(self, obj):
+
+        if self.username.text is "":
+            check_string = "Please enter a username"
+
+        else:
+            check_string = self.username.text + " does not exist"
+
+        close_btn = MDFlatButton(text="close", on_release=self.close_dialog)
+        more_btn = MDFlatButton(text="more")
+        self.dialog = MDDialog(title="Username Check",
+                               text=check_string,
+                               size_hint=(0.7, 1),
+                               buttons=[close_btn, more_btn])
+        self.dialog.open()
+
+    def close_dialog(self, obj):
+        self.dialog.dismiss()
+
+        self.dialog.open()
+        print(self.username.text)
 
 
 if __name__ == "__main__":
