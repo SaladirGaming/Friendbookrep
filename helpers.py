34a348d6d44aff0fc950f803cbe2c172948b714d
SaladirGaming <saladirgaming@gmail.com>
2022-06-12 23:47:16 +0200

    Login GUI
    Adding Login GUI without functionality

---
+++ helpers.py
@@ -0,0 +1,16 @@
+username_helper = """
+MDTextField:
+    hint_text: "Enter Username"
+    pos_hint: {"center_x": 0.5, "center_y": 0.5}
+    size_hint_x: None
+    width: 300
+"""
+
+password_helper = """
+MDTextField:
+    hint_text: "Enter Password"
+    pos_hint: {"center_x": 0.5, "center_y": 0.4}
+    size_hint_x: None
+    width: 300
+    password: True
+"""
